d306497ab951469eb03df71acce2fd239be28eb9
carla cunha <marlenecunha14@hotmail.com>
2017-11-05 23:48:16 +0000

    Mvc

---
+++ McMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace McMovie.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<McMovie.Models.Movie> Movie { get; set; }
+    }
+}
